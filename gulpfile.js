3f0aa3babdbc38b8653dcef8acb8f4035b880cc6
Marcamillian <marcamillian@gmail.com>
2018-05-05 12:25:42 +0100

    chore: Add html and css build steps

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 
 
-gulp.task('default', ()=>{
+gulp.task('default', ['js', 'css', 'html'] ,()=>{
 
 })
 
@@ -12,4 +12,14 @@ gulp.task('js', ()=>{
       .bundle()
       .pipe(source('bundle.js'))
       .pipe(gulp.dest(('./dist-gulp/js')))
+})
+
+gulp.task('html',()=>{
+  gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('dist-gulp'))
+})
+
+gulp.task('css',()=>{
+  gulp.src('./src/css/**/**.css')
+    .pipe(gulp.dest('dist-gulp/css'))
 })
